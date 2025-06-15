b11d7f64107e55b2fbb182e0175ee6c12c6fb24b
Leopoldo Roa <166873477+LeopoldoRoa08@users.noreply.github.com>
2025-06-14 21:27:06 -0400

    Instalacion react
    a

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Home</div>,
+    },
+    {
+      path: "/about",
+      element: <div>About</div>,
+    },
+  ]);
+
+  return (
+    <RouterProvider router={router}/>
+
+   
+  )
+}
+
+export default App
