61feec6049aa4370bb69cf64d1cde4a248fb4130
Quirikocho <jmmoreiraramos@gmail.com>
2025-07-04 13:28:37 -0400

    o

--- src/CrearEspacio/EspaciosDisponiblesCrear.jsx
+++ src/CrearEspacio/EspaciosDisponiblesCrear.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from 'react';
-import  './CrearEspacio.css';
+import styles from './EspaciosDisponiblesCrear.module.css';
 
 function EspaciosDisponiblesCrear() {
   return (
