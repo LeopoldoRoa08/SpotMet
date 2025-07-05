72939d008b7c83a797b8cdfa456433ee81addb60
Leopoldo Roa <166873477+LeopoldoRoa08@users.noreply.github.com>
2025-07-04 22:54:10 -0400

    funcionaa

--- src/Reserva/ReservaAuditorio.jsx
+++ src/Reserva/ReservaAuditorio.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import "./ReservaAuditorio.css";
 
 const ReservaAuditorio = ({ color = "#EE5D08", className }) => {
